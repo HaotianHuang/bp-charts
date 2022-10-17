324e4e59f109f1efe8e01aa5eeaca704fe78cf24
Haotian Huang <haotianhuang@gmail.com>
2022-10-17 11:50:48 +1100

    display patient name

--- load_data.js
+++ load_data.js
@@ -1,6 +1,23 @@
 (function(window) {
     window.extractData = function() {
 
+        function makeArray(x) {
+            if (!Array.isArray(x)) {
+              return [x];
+            }
+            return x;
+          }
+
+        function getPatientName(patient) {
+            var name  = makeArray(patient.name);
+            var fname = makeArray(name[0].given  || "").join(" ");
+            var lname = makeArray(name[0].family || "").join(" ");
+            return {
+              given : fname,
+              family: lname
+            };
+          }
+
         return FHIR.oauth2.ready().then(function(client){
             
             // Fetch the patient
@@ -17,7 +34,11 @@
             return Promise.all([patientQuery, labsQuery]).then(function(data){
 
                 console.log(data[0]);
-                return data[0];
+                var patient = data[0];
+
+                var name = getPatientName(patient);
+
+                return name;
 
             })
 
